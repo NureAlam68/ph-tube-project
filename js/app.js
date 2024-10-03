e04665520fa5f3658cd4db7eacb0035a04db99c7
NureAlam68 <nurealam151068@gmail.com>
2024-10-03 18:07:28 +0600

    added thumbnail using api

--- js/app.js
+++ js/app.js
@@ -3,12 +3,51 @@
 // create loadCategories
 
 const loadCategories = () => {
-  // fetch the data
-  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then((res) => res.json())
-    .then((data) => displayCategories(data.categories))
-    .catch((error) => console.log(error));
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
+        .catch((error) => console.log(error));
 };
+
+
+
+// create load videos
+
+const loadVideos = () => {
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error));
+};
+
+// create display videos
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    // console.log(videos);
+    videos.forEach((video) => {
+        console.log(video);
+        const card = document.createElement('div');
+        card.classList = "card card-compact";
+        card.innerHTML = `
+     <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+        videoContainer.append(card);
+    });
+}
 
 // create displayCategories
 const displayCategories = (categories) => {
@@ -28,4 +67,7 @@ const displayCategories = (categories) => {
     });
 };
 
+
+
 loadCategories();
+loadVideos();
